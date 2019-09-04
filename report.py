58d562922c92eceaa5bb330ce67d3ce597d9fd37
ShengjiaZhao <sjzhao@stanford.edu>
2019-09-04 00:47:49 -0700

    Add pie chart

--- report.py
+++ report.py
@@ -4,6 +4,7 @@ from pandas import isnull
 from datetime import datetime
 import pandas as pd
 from matplotlib import pyplot as plt
+import seaborn as sns
 import os
 import argparse
 import math
@@ -11,7 +12,7 @@ import math
 parser = argparse.ArgumentParser()
 parser.add_argument("--path", type=str, default='OmniFocus.csv', help="The name to the csv file")
 parser.add_argument("--timezone", type=str, default='US/Pacific')
-parser.add_argument("--target", type=float, default=4, help="Target work hours per day")
+parser.add_argument("--target", type=float, default=7, help="Target work hours per day")
 args = parser.parse_args()
 print(args)
 
@@ -68,6 +69,7 @@ class TreeNode:
 
     def clean_tree(self):
         # This should only be called by root note
+        # Complete any missing due dates
         self.inherit_due(
             datetime.strptime("2019-12-31 11:59:00 +0000", '%Y-%m-%d %H:%M:%S %z').astimezone(self.timezone))
 
@@ -100,12 +102,14 @@ class TreeNode:
     def compute_completion(self):
         # This is a leaf node
         if len(self.children) == 0:
+            # If the action has not been completed, add it to future tasks times
             if not self.completion_date:
                 self.completed_time = 0
                 days_away = (self.due_date.date() - datetime.now(self.timezone).date()).days
                 self.earliest_due = days_away
                 # print(days_away, self.due_date.date(), datetime.now(self.timezone).date())
                 return None, np.array([[days_away, self.total_time]], dtype=np.int)
+            # If the action is completed, add it to past tasks times
             else:
                 self.completed_time = self.total_time
                 self.earliest_due = 0
@@ -141,33 +145,103 @@ class TreeNode:
             return completion_list, due_list
 
     # Report generation
-
-    def plot_completion(self, ylim1=None):
+    def plot_completion(self, ylim1=None, date_range=None):
         ax1 = plt.gca()
         ax2 = ax1.twinx()
-        xaxis = [-i for i in range(len(self.completion_pdf))]
-        ax2.bar(xaxis, np.minimum(self.completion_pdf, 24), alpha=0.5, color='m')
+        if date_range is None:
+            pdf = self.completion_pdf
+            cdf = self.completion_cdf
+            date_range = len(self.completion_cdf)
+        else:
+            date_range = min(date_range, len(self.completion_pdf))
+            pdf = self.completion_pdf[:date_range]
+            cdf = self.completion_cdf[:date_range]
+            cdf -= self.completion_cdf[date_range]
+        xaxis = [-i for i in range(len(pdf))]
+        ax2.bar(xaxis, np.minimum(pdf, 24), alpha=0.5, color='m')
         ax2.set_ylabel('Hours by Day', fontsize=14)
 
-        ax1.plot(xaxis, self.completion_cdf, linewidth=3)
+        ax1.plot(xaxis, cdf, linewidth=3)
         ax1.set_xlabel('Days ago', fontsize=14)
         ax1.set_ylabel('Cumulative Hours', fontsize=14)
 
         if self.level == 0:
             if len(xaxis) > 30:
-                ax1.plot(xaxis[:30], self.completion_cdf[30] + np.flip(np.array([i * args.target for i in range(30)]), 0),
+                ax1.plot(xaxis[:30], cdf[30] + np.flip(np.array([i * args.target for i in range(30)]), 0),
                          color='g', linewidth=3, linestyle=':', alpha=0.8)
             if len(xaxis) > 7:
-                ax1.plot(xaxis[:7], self.completion_cdf[7] + np.flip(np.array([i * args.target for i in range(7)]), 0),
+                ax1.plot(xaxis[:7], cdf[7] + np.flip(np.array([i * args.target for i in range(7)]), 0),
                          color='g', linewidth=3, linestyle=':', alpha=1.0)
-            ax1.plot(xaxis, np.flip(np.array([i * args.target for i in range(len(self.completion_pdf))]), 0),
-                     color='g', alpha=0.6, linewidth=3, linestyle=':', label='Reference %.1fh/day' % args.target)
-            ax2.axhline(args.target, linestyle=':', color='m')
-
+            ax1.plot(xaxis, np.flip(np.array([i * args.target for i in range(len(pdf))]), 0),
+                     color='g', alpha=0.6, linewidth=3, linestyle=':')
+            ax2.axhline(args.target, linestyle=':', color='g', label='Reference %.1fh/day' % args.target)
+            ax2.axhline(cdf[0] / date_range, linestyle=':', color='m', label='Actual %.1fh/day' % (cdf[0] / date_range))
         if ylim1 is not None:
             ax1.set_ylim([0, ylim1])
         if self.level == 0:
-            ax1.legend()
+            ax2.legend()
+
+    def plot_category(self, time_range=None):
+        num_color = 20
+
+
+        items = {}
+        total_time = 0
+        for index in self.children:
+            cdf = self.children[index].completion_cdf
+            if len(cdf) != 0:
+                if time_range is not None and len(cdf) > time_range:
+                    sub = cdf[time_range]
+                else:
+                    sub = 0.0
+                items[self.children[index].name] = cdf[0] - sub
+                total_time += cdf[0] - sub
+
+        # Assign a unique color to each item
+        palette = sns.color_palette("husl", len(items))
+        color_dict = {}
+        for i, key in enumerate(items):
+            color_dict[key] = palette[i]
+
+        threshold = total_time / 20.0
+        labels = ['Others']
+        values = [0.0]
+        colors = ['gray']
+        for key in items:
+            # If text too long line break it
+            if len(key) < 15 or key.find(' ') == -1:
+                name = key
+            else:
+                prev_find = -1
+                while True:
+                    cur_find = key.find(' ', prev_find + 1)
+                    if cur_find == -1:
+                        break
+                    prev_find = cur_find
+                    if cur_find > len(key) / 2.0:
+                        break
+                name = key[:prev_find] + '\n' + key[prev_find + 1:]
+
+            if items[key] > threshold:
+                labels.append(name)
+                values.append(items[key])
+                colors.append(color_dict[key])
+            else:
+                values[0] += items[key]
+
+        if values[0] < 0.1:
+            labels = labels[1:]
+            values = values[1:]
+
+        ax1 = plt.gca()
+
+        def func(pct):
+            absolute = int(pct / 100. * total_time)
+            return "{:.0f}%\n({:d} h)".format(pct, absolute)
+
+        ax1.pie(values, labels=labels, autopct=lambda pct: func(pct),
+                shadow=False, startangle=90, colors=colors)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     def plot_due(self, date_range=7):
         if date_range > self.earliest_due + len(self.due_pdf):
@@ -191,26 +265,58 @@ class TreeNode:
             ax2.axhline(args.target, linestyle=':', c='m')
 
     def generate_report(self, depth=0, parent_name="Reports"):
-        plt.figure(figsize=(20, 5))
-        plt.subplot(1, 3, 1)
-        plt.title("Completed Time")
+        from matplotlib import rcParams
+        rcParams['axes.titlepad'] = 10
+
+        plt.figure(figsize=(21, 12))
+        plt.subplot(2, 3, 1)
+        plt.title("Completed Time (Total)")
         try:
             self.plot_completion()
         except:
             plt.cla()
-        plt.subplot(1, 3, 2)
+
+        plt.subplot(2, 3, 2)
+        plt.title("Completed Time (Past 30 Days)")
+        try:
+            self.plot_completion(date_range=30)
+        except:
+            plt.cla()
+
+        plt.subplot(2, 3, 3)
         plt.title("Due Work (Week)")
         try:
             self.plot_due(7)
         except:
             plt.cla()
-        plt.subplot(1, 3, 3)
-        plt.title("Due Work (Month)")
+
+        plt.subplot(2, 3, 4)
+        plt.title("Time by Category (Total)")
         try:
-            self.plot_due(30)
+            self.plot_category()
         except:
             plt.cla()
-        plt.tight_layout()
+
+        plt.subplot(2, 3, 5)
+        plt.title("Time by Category (Past 30 Days)")
+        try:
+            self.plot_category(time_range=30)
+        except:
+            plt.cla()
+
+        plt.subplot(2, 3, 6)
+        plt.title("Time by Category (Past 7 Days)")
+        try:
+            self.plot_category(time_range=7)
+        except:
+            plt.cla()
+        # plt.subplot(1, 3, 3)
+        # plt.title("Due Work (Month)")
+        # try:
+        #     self.plot_due(30)
+        # except:
+        #     plt.cla()
+        plt.tight_layout(pad=5.0, w_pad=5.0)
 
         if not os.path.isdir(parent_name):
             os.makedirs(parent_name)
